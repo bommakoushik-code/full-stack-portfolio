c5d027d7a6e9452f790bd5cb508ff86c63ad182f
Koushik Bomma <bommakoushik@gmail.com>
2026-06-09 16:39:08 +0530

    Connect frontend to deployed backend

--- portfolio-react/src/App.jsx
+++ portfolio-react/src/App.jsx
@@ -27,7 +27,7 @@ const handleSubmit = async (e) => {
 
 
   try {
-    const response = await fetch("http://localhost:5000/api/contact", {
+    const response = await fetch("https://full-stack-portfolio-qp1x.onrender.com/api/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
